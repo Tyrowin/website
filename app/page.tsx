05b39458c5d4bd98d09ba098b87ee95c5aa2f765
Malte <112257731+Tyrowin@users.noreply.github.com>
2024-05-03 10:01:02 +0200

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,19 @@ export default function Page() {
                 Malte Mindedal | Developer
             </h1>
             <p className="mb-4">
-                {`I'm a student and new developer from Sweden. I mostly working with Python and a little C++.
+                {`I'm a student and new developer from Sweden. I mostly working with Python and some C++.
                   I enjoy learning new things and I'm always looking for new projects to work on.
                 `}
             </p>
             <p className="mb-4">
                 {`Im currently an intern at Stena Fastigheter in Gothenburg, 
-                  Sweden. Where i've been working mostly with Python and Azure Cloud Services.
-                 
+                  Sweden. Where i've been working mostly with Python and Azure Cloud Services. 
                 `}
             </p>
             <p className="mb-4">
-                Want to know more? Use the links below to find me on social
+                {`Want to know more? Use the links below to find me on social
                 media, read about my projects or check out my resume.
+                `}
             </p>
             <div className="my-8">
                 <BlogPosts />
